4460e43954a65f7b19e8a3a2e9def4b6e3b82438
Frank Lai <frankc201106@gmail.com>
2017-04-12 21:56:06 -0400

    Finished hashing/longest_substr_wo_repeat

---
+++ interviewbit/hashing/longest_substr_wo_repeat/main.cc
@@ -0,0 +1,53 @@
+/*
+ * Created on 2017-04-10.
+ * -------------------------------------------------------------------------------
+ * Question: Given a string,
+ * find the length of the longest substring without repeating characters.
+ * -------------------------------------------------------------------------------
+ * Examples:
+ * Input: abcacbbb -> abc
+ * Output: 3
+ * -------------------------------------------------------------------------------
+ * Note:
+ * Time taken to solve: 94 min. / Average Solving Time: 37 min.
+ * Complexities:
+ *      Time: O(n) avg and O(n^2) worst for when all chars are unique and hashed to the same bucket
+ *      Space: O(n)
+ * Idea: Traverse the string character by character. If we haven't visited this character before, we know
+ * it's part of a non-repeating substring
+ * If we have, compute the length of the current non-repeating substring by keep get max(lastseenIndex(current), index
+ * of the character last repeated).
+ * -------------------------------------------------------------------------------
+ */
+
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+int lengthOfLongestSubstring(string a) {
+    int longest = 0;
+    unordered_map<char, int> myMap({});
+    int current = 0;
+    int lastRepeatedIndex = 0;
+    for (int i=0; i<a.size(); ++i) {
+        char c = a[i];
+        if (myMap.count(c) == 0) {
+            if (++current > longest) {
+                longest=current;
+            }
+        } else {
+            int lastSeenIndex = myMap.find(c)->second;
+            if (lastSeenIndex > lastRepeatedIndex) {lastRepeatedIndex = lastSeenIndex;}
+            current = i - lastRepeatedIndex;
+            if (current>longest) {
+                longest=current;
+            }
+        }
+        myMap[c] = i;
+    }
+    return longest;
+}
+int main() {
+    string input = "Wnb9z9dMc7E8v1RTUaZPoDNIAXRlzkqLaa97KMWLzbitaCkRpiE4J4hJWhRcGnC8H6mwasgDfZ76VKdXhvEYmYrZY4Cfmf4HoSlchYWFEb1xllGKyEEmZOLPh1V6RuM7Mxd7xK72aNrWS4MEaUmgEn7L4rW3o14Nq9l2EN4HH6uJWljI8a5irvuODHY7A7ku4PJY2anSWnfJJE1w8p12Ks3oZRxAF3atqGBlzVQ0gltOwYmeynttUmQ4QBDLDrS4zn4VRZLosOITo4JlIqPD6t4NjhHThOjJxpMp9fICkrgJeGiDAwsb8a3I7Txz5BBKV9bEfMsKNhCuY3W0ZHqY0MhBfz1CbYCzwZZdM4p65ppP9s5QJcfjadmMMi26JKz0TVVwvNA8LP5Vi1QsxId4SI19jfcUH97wmZu0pbw1zFtyJ8GAp5yjjQTzFIboC1iRzklnOJzJld9TMaxqvBNBJKIyDjWrdfLOY8FGMOcPhfJ97Dph35zfxYyUf4DIqFi94lm9J0skYqGz9JT0kiAABQZDazZcNi80dSSdveSl6h3dJjHmlK8qHIlDsqFd5FMhlEirax8WA0v3NDPT8vPhwKpxcnVeu14Gcxr3h1wAXXV0y7Xy9qqB2NQ5HQLJ7cyXAckEYHsLCPSy28xcdNJatx1KLWohOQado4WywJbGvsFR17rKmvOPABweXnFD3odrbSMD4Na4nuBBswvMmFRTUOcf7jZi4z5JnJqXz6hitaPnaEtjoSEBq82a52nvqYy7hhldBoxen2et2OMadVEHeTYLL7GLsIhTP6UizHIuzcJMljo4lFgW5AyrfUlIBPAlhwaSiJtTvcbVZynDSM6RO1PqFKWKg2MHIgNhjuzENg2oFCfW7z5KJvEL9qWqKzZNc0o3BMRjS04NCHFvhtsteQoQRgz84XZBHBJRdekCdcVVXu9c01gYRAz7oIAxN3zKZb64EFKssfQ4HW971jv3H7x5E9dAszA0HrKTONyZDGYtHWt4QLhNsIs8mo4AIN7ecFKewyvGECAnaJpDn1MTTS4yTgZnm6N6qnmfjVt6ZU51F9BxH0jVG0kovTGSjTUkmb1mRTLQE5mTlVHcEz3yBOh4WiFFJjKJdi1HBIBaDL4r45HzaBvmYJPlWIomkqKEmQ4rLAbYG7C5rFfpMu8rHvjU7hP0JVvteGtaGn7mqeKsn7CgrJX1tb8t0ldaS3iUy8SEKAo5IZHNKOfEaij3nI4oRVzeVOZsH91pMsA4jRYgEohubPW8ciXwVrFi1qEWjvB8gfalyP60n1fHyjsiLW0T5uY1JzQWHKCbLVh7QFoJFAEV0L516XmzIo556yRH1vhPnceOCjebqgsmO78AQ8Ir2d4pHFFHAGB9lESn3OtJye1Lcyq9D6X93UakA3JKVKEt6JZDLVBMp4msOefkPKSw59Uix9d9kOQm8WCepJTangdNSOKaxblZDNJ5eHvEroYacBhd9UdafEitdF3nfStF7AhkSfQVC61YWWkKTNdx96OoJGTnxuqt4oFZNFtO7aMuN3IJAkw3m3kgZFRGyd3D3wweagNL9XlYtvZwejbjpkDOZz33C0jbEWaMEaUPw6BG49XqyQoUwtriguO0yvWyaJqD4ye3o0E46huKYAsdKAq6MLWMxF6tfyPVaoqOGd0eOBHbAF89XXmDd4AIkoFPXkAOW8hln5nXnIWP6RBbfEkPPbxoToMbV";
+    lengthOfLongestSubstring(input);
+}
